c89cbb7f308d7bbb823e82536a9b4c54dbade6de
oleg <gerbylev.oleg@gmail.com>
2024-05-27 11:51:11 +0300

    add front

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from flask import Flask, request, jsonify, g
 from db import get_db
+from flask_cors import CORS, cross_origin
 import service.auth as authService
 import service.file as fileService
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/register', methods=['POST'])
+@cross_origin()
 def register():
     return authService.register()
 
 
 @app.route('/login', methods=['POST'])
+@cross_origin()
 def login():
     return authService.login()
 
 @app.route('/sendDocument', methods=['POST'])
+@cross_origin()
 def sendDocument():
     return fileService.sendDocx()
 
